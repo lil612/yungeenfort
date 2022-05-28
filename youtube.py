3579bc5b96c0b45e575aad3b0628d608f25597e9
Ridley Neslon <ridley.nelson@outlook.com>
2022-05-28 18:35:20 -0500

    Update

---
+++ youtube.py
@@ -0,0 +1,19 @@
+import youtube_dl, os
+def run(LINK):
+    video_url = LINK
+    video_info = youtube_dl.YoutubeDL().extract_info(url = video_url,download=False)
+    filename = f"{video_info['title']}.mp3"
+    options={'format':'bestaudio/best', 'keepvideo':False, 'outtmpl':filename,}
+
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download([video_info['webpage_url']])
+
+    print("Download complete... {}".format(filename))
+    os.system("cls")
+
+if __name__=='__main__':
+    queue = [
+        
+    ]
+    for video in queue:
+        run(video)
